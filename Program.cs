876687b943fc8fdbdbe4c13f07d4a975bcfd41fd
Renan Ramos <90880803+Ryotruuser@users.noreply.github.com>
2021-12-11 00:41:17 -0300

    adicionando logica do menu como loop while

--- Program.cs
+++ Program.cs
@@ -6,7 +6,49 @@ namespace MFGrades
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string opcaoUsuario = MenuUserOption();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO adicionar aluno
+                        break;
+                    case "2":
+                        //TODO listar alunos
+                        break;
+                    case "3":
+                        //TODO  calcular media geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = MenuUserOption();
+                
+            }
+
+        }
+
+        private static string MenuUserOption()
+        {
+            Console.WriteLine("=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine();
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine();
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine();
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
